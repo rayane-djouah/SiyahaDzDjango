5f6a163bb25071bfbe218773172c279a77a21602
Rayane Djouah <kr_djouah@esi.dz>
2023-05-15 09:51:52 +0100

    db configuration

--- SiyahaDzProject/settings.py
+++ SiyahaDzProject/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "SiyahaDzProject.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ET_DB',
+        'USER': 'root',
+        'PASSWORD': 'Rayane123##',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
